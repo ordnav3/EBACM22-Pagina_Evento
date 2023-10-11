1ac14822f9e4995aa5c10bb41b7a7a334d6c3597
Evandro Santos Afonso <evandrosantosafonso@live.com>
2023-10-11 10:35:44 -0300

    criando a contagem do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('Olá Mundo')
+AOS.init();
+
+const dataDoEvento = new Date("Apr 26, 2027 19:30:00");
+const timesTempDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(() => {
+    const agora = new Date();
+    const timesTempAtual = agora.getTime();
+
+    const distanciaAteOEvento = timesTempDoEvento - timesTempAtual
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+
+    const minutosAteOEvento = Math.floor((diasAteOEvento % horaEmMs) / minutoEmMs);
+
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000)
+
+
+
+    const contador = document.getElementById('contador')
+
+    contador.innerHTML = `${diasAteOEvento} dias ${horasAteOEvento} horas ${minutosAteOEvento} minutos ${segundosAteOEvento} segundos `;
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        contador.innerHTML = 'Evento Expirado'
+    }
+
+}, 1000);
